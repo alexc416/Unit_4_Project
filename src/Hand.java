131b7dad02a6e82c70e2cf7a6eacfd39d34640dd
alexc416 <alexanderc416@nycstudents.net>
2026-02-04 14:48:40 -0500

    Add basic part 1

--- src/Hand.java
+++ src/Hand.java
@@ -4,6 +4,7 @@ public class Hand {
     private String[] cards;
     private int bid;
     private int handType;
+    private int[] listOfAmounts;
 
     Hand(String[] c, int b) {
         cards = c;
@@ -11,13 +12,91 @@ public class Hand {
         handType = 0;
     }
 
-    public void findHandType(Hand a) {
-
-
-
-    }
-
     public void printHand() {
         System.out.println(Arrays.toString(cards) + "\n" + bid + " " + handType);
+    }
+
+    public String[] getCards() {
+        return cards;
+    }
+
+    public void setHandType(int e) {
+        handType = e;
+    }
+    public void setListOfAmounts(int[] in) {
+        listOfAmounts = in;
+    }
+
+    public boolean isFiveOfKind() {
+        for (int a : listOfAmounts) {
+            if (a == 5) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isFourOfKind() {
+        for (int a : listOfAmounts) {
+            if (a == 4) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isFullHouse() {
+        boolean hasThree = false;
+        boolean hasPair = false;
+        for (int a : listOfAmounts) {
+            if (a == 3) {
+                hasThree = true;
+            }
+            if (a == 2) {
+                hasPair = true;
+            }
+        }
+        if (hasThree && hasPair) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isThreeOfKind() {
+        for (int a : listOfAmounts) {
+            if (a == 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isTwoPair() {
+        int numOfPairs = 0;
+        for (int a : listOfAmounts) {
+            if (a == 2) {
+                numOfPairs += 1;
+            }
+        }
+        if (numOfPairs == 2) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isOnePair() {
+        int numOfPairs = 0;
+        for (int a : listOfAmounts) {
+            if (a == 2) {
+                numOfPairs += 1;
+            }
+        }
+        if (numOfPairs == 1) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Hand[] hands = new Hand[5];
+        Hand[] hands = new Hand[79];
+        //read over the file, count lines, institude array with that many lines, IMPLEMENT
         int i = 0;
+
+        //PARSER
         try {
             File f = new File("src/input");
             Scanner s = new Scanner(f);
 
             while (s.hasNextLine()) {
                 String line = s.nextLine();
-                String[] hand = line.split("\\|");
-                String[] cards = hand[0].split(",");
+                String[] wholeHand = line.split("\\|");
+                String[] cards = wholeHand[0].split(",");
 
-                Hand h = new Hand(cards, Integer.parseInt(hand[1]));
+                Hand h = new Hand(cards, Integer.parseInt(wholeHand[1]));
                 hands[i] = h;
                 i++;
             }
@@ -24,11 +27,77 @@ public class Main {
             System.out.println("error no file found...");
         }
 
+        //HAND IDENTIFIER
         for (Hand h : hands) {
-            h.printHand();
+            int[] cardAmounts = new int[13];
+            for (String card : h.getCards()) {
+                for (int k = 0; k < 14; k++) {
+                    if (k == 1) {
+                        if (card.equals("Ace")) {
+                            cardAmounts[0] += 1;
+                        }
+                    } else if (k == 11) {
+                        if (card.equals("Jack")) {
+                            cardAmounts[10] += 1;
+                        }
+                    } else if (k == 12) {
+                        if (card.equals("Queen")) {
+                            cardAmounts[11] += 1;
+                        }
+                    } else if (k == 13) {
+                        if (card.equals("King")) {
+                            cardAmounts[12] += 1;
+                        }
+                    } else {
+                        if (card.equals(k + "")) {
+                            cardAmounts[k-1] += 1;
+                        }
+                    }
+                }
+            }
+            h.setListOfAmounts(cardAmounts);
         }
 
+        //PART 1
+        int[] cardHands = new int[7];
+        for (Hand h : hands) {
+            //five, full house, hour, three, two, one, high
 
+            if (h.isFiveOfKind()) {
+                cardHands[0] += 1;
+            } else if (h.isFullHouse()) {
+                cardHands[1] += 1;
+            } else if (h.isFourOfKind()) {
+                cardHands[2] += 1;
+            } else if (h.isThreeOfKind()) {
+                cardHands[3] += 1;
+            } else if (h.isTwoPair()) {
+                cardHands[4] += 1;
+            } else if (h.isOnePair()) {
+                cardHands[5] += 1;
+            } else {
+                cardHands[6] += 1;
+            }
+        }
+        for (int c = 0; c < 7; c++) {
+            String out = "";
+            if (c == 0) {
+                out += "Number of five of a kind hands: ";
+            } else if (c == 1) {
+                out += "Number of full house hands: ";
+            } else if (c == 2) {
+                out += "Number of four of a kind hands: ";
+            } else if (c == 3) {
+                out += "Number of three of a kind hands: ";
+            } else if (c == 4) {
+                out += "Number of two pair hands: ";
+            } else if (c == 5) {
+                out += "Number of one pair hands: ";
+            } else {
+                out += "Number of high card: ";
+            }
+            System.out.println(out + cardHands[c]);
+        }
 
     }
 }
